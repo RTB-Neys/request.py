bd8aae86f1d5fd58bb5299328357b5ab7e3b8fcb
Neysalee Acosta <nacosta@clickpay.com>
2020-10-04 23:55:36 -0400

    Javascript code still not working

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # request.py
 
-Created a software tool that generates random and cutomized passwords for users with a length between 8 and 128, including special characters, uppercase, lowercase and numeric values.  
+PASSWORD GENERATOR
+
+Description: Create a password after the user is prompted to define criteria. 
+
+When the user clicks the button "Generate Password" they are first promoted for password length. If they select a range outside of 8-128, the user will be advised to select a number within range. 
+
+The program will prompt a selection of numbers, characters, uppercase and/or lowercase letters. If the user selects none to all criteria, they will be advised to select at least each one.
+
+https://rtb-neys.github.io/request.py/
--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+
+var length = Number(prompt("How many characters would you like your password to be?")); {
+while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+
+var upperChars = confirm("Would you like to use uppercase letters?");
+var lowerChars = confirm("Would you like to use lowercase letters?");
+var numberChars = confirm("Would you like to use numbers?");
+var symbols = confirm("Would you like to use special characters?");
+}
+
+while (!uppers && !lowers && !numbers && !symbols) {
+  alert("You must select at least one character type!");
+  uppers = confirm("Would you like to use uppercase letters?");
+  lowers = confirm("Would you like to use lowercase letters?");
+  numbers = confirm("Would you like to use numbers?");
+  symbols = confirm("Would you like to use special characters?");
+}
+
+window.addEventListener('load', function() {
+  generate();
+});
+
+function generate() {
+  var password = "";
+
+  var allowed = {};
+  if (upperChars) password += rando(allowed.upperChars = "QWERTYUIOPASDFGHJKLZXCVBNM");
+  if (lowerChars) password += rando(allowed.lowerChars = "qwertyuiopasdfghjklzxcvbnm");
+  if (numberChars) password += rando(allowed.numberChars = "1234567890");
+  if (symbolChars) password += rando(allowed.symbolChars = "!@#$%^&*(){}[]=<>/,.");
+
+  for (var i = password.length; i < length; i++) password += rando(rando(allowed).value);
+
+  document.getElementById("password").value = randoSequence(password).join("");
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
